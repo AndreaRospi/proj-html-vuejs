7210cdee137ee2811bd749dd1454c4674d76d3ff
Andrea <andr3z45@gmail.com>
2022-01-14 06:12:38 +0100

    jumbo completato

---
+++ src/components/elements/Jumbo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <img src="../../assets/img/home_finance_slider_bg.jpg" alt="" />
+    <h1>Right Peoples<span>.</span> Right Investments<span>.</span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbo",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/global.scss";
+.container {
+  position: relative;
+  img {
+    width: 100%;
+  }
+  h1 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    font-weight: 400;
+    font-size: 70px;
+    span {
+      color: #5ddecc;
+    }
+  }
+}
+</style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,12 +1,19 @@
 <template>
-  <main></main>
+  <main>
+    <Jumbo />
+  </main>
 </template>
 
 <script>
+import Jumbo from "../elements/Jumbo.vue";
 export default {
   name: "main",
+  components: {
+    Jumbo,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/style/global.scss";
 </style>
